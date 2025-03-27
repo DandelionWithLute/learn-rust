0ae23371f7c0289f29e19b678d7bc6225a779869
Dandelion <114991717+DandelionWithLute@users.noreply.github.com>
2025-03-27 22:20:53 +0800

    first 3 chapters
    https://www.youtube.com/watch?v=rQ_J9WH6CGk

---
+++ src/2compound.rs
@@ -0,0 +1,34 @@
+fn compound() {
+    // 1 - Debuggable format
+    // 2 - Display format
+    //  {:?}
+    let numbers: [i32; 5] = [1, 2, 3, 4, 5];
+    println!("Number Array:{:?}", numbers);
+
+    // let mix: [i32; 4] = [1, 2, "apple", true];
+    // println!(mix)
+
+    let fruits: [&str; 3] = ["Apple", "Banana", "Orange"];
+    println!("Fruits Array:{:?}", fruits);
+    println!("Fruits:{:?}", fruits[0]);
+    println!("Fruits:{:?}", fruits[1]);
+    println!("Fruits:{:?}", fruits[2]);
+    println!("Fruits:{}", fruits[0]);
+    println!("Fruits:{}", fruits[1]);
+    println!("Fruits:{}", fruits[2]);
+
+    // Tuples
+    // let human: (String) = ("Alice", 30, false);
+    // let human: (&str, i32, bool) = ("Alice", 30, false);
+    let human: (String, i32, bool) = ("Alice".to_string(), 30, false);
+    println!("Human Tuple :{:?}", human);
+
+    let mut stone_cold: String = String::from("Hell, ");
+    stone_cold.push_str("Yeah!");
+    println!("Stone Clod Says: {}", stone_cold);
+
+    // slice
+    let string: String = String::from("Hello, World!");
+    let slice: &str = &string[0..5];
+    println!("Slice Value: {}", slice)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,11 @@
 fn main() {
-    // 1 - Debuggable format
-    // 2 - Display format
-    //  {:?}
-    let numbers: [i32; 5] = [1, 2, 3, 4, 5];
-    println!("Number Array:{:?}", numbers);
+    let old = 12;
+    let young = 14;
+    let total = add(old, young);
+    println!("All persons in total are: {}", total);
+    println!("All persons in total are: {}", add(old, young));
+}
 
-    // let mix: [i32; 4] = [1, 2, "apple", true];
-    // println!(mix)
-
-    let fruits: [&str; 3] = ["Apple", "Banana", "Orange"];
-    println!("Fruits Array:{:?}", fruits);
-    println!("Fruits:{:?}", fruits[0]);
-    println!("Fruits:{:?}", fruits[1]);
-    println!("Fruits:{:?}", fruits[2]);
-    println!("Fruits:{}", fruits[0]);
-    println!("Fruits:{}", fruits[1]);
-    println!("Fruits:{}", fruits[2]);
-
-    // Tuples
-    // let human: (String) = ("Alice", 30, false);
-    // let human: (&str, i32, bool) = ("Alice", 30, false);
-    let human: (String, i32, bool) = ("Alice".to_string(), 30, false);
-    println!("Human Tuple :{:?}", human);
-
-    let mut stone_cold: String = String::from("Hell, ");
-    stone_cold.push_str("Yeah!");
-    println!("Stone Clod Says: {}", stone_cold);
-
-    // slice
-    let string: String = String::from("Hello, World!");
-    let slice: &str = &string[0..5];
-    println!("Slice Value: {}", slice)
+fn add(a: i32, b: i32) -> i32 {
+    a + b
 }
